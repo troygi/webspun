2a22657782e9205587858c1806dc53aa4b4e7735
Troy Gilliland <troygi@mac.com>
2020-05-03 14:05:29 -0400

    Updates

---
+++ precache-manifest.e3729ea44d6df9fdbf8962b8bb9b2361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2879eb09362a6c9840eb24849cebd531",
+    "url": "/webspun/index.html"
+  },
+  {
+    "revision": "129bf1ea32b0dcf7cc60",
+    "url": "/webspun/static/css/main.a4991974.chunk.css"
+  },
+  {
+    "revision": "e026acaaf5f66c5fabfc",
+    "url": "/webspun/static/js/2.83c8ac7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webspun/static/js/2.83c8ac7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129bf1ea32b0dcf7cc60",
+    "url": "/webspun/static/js/main.142fa85e.chunk.js"
+  },
+  {
+    "revision": "3daaeca9418518430271",
+    "url": "/webspun/static/js/runtime-main.8f603db6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webspun/static/media/logo.5d5d9eef.svg"
+  }
+]);
